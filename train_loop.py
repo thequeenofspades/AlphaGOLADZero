26faed6247405ae03280cfb04dddbb6093734dbf
Eli <ewang921@gmail.com>
2018-02-27 12:26:27 -0800

    file for training nn using data from mcts self-play

---
+++ train_loop.py
@@ -0,0 +1,21 @@
+import numpy as np
+from nn.nn import NN
+from config import config
+from mcts import UCTPlayGame
+
+if __name__ == "__main__":
+    """ Play a single game to the end using UCT for both players. 
+    """    
+    nn = NN(config)
+    nn.setup()
+    batch_data = {}
+    batch_data['s'], batch_data['pi'], batch_data['z'] = ([], [], [])
+    while len(batch_data['s']) < config.batch_size:
+        if config.verbose:
+            print('Current data size: {}'.format(len(batch_data['s'])))
+        data = UCTPlayGame(nn)
+        for k in batch_data.keys():
+            batch_data[k].extend(data[k])
+    
+    batch_sample = (batch_data['s'][:config.batch_size], batch_data['pi'][:config.batch_size], batch_data['z'][:config.batch_size])
+    nn.train(batch_sample)
