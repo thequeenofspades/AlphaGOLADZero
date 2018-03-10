16ca6e272bf94c93745c8cc26736f39c2c20b025
Eli <ewang921@gmail.com>
2018-03-09 16:14:20 -0800

    add saving data batch

--- train_loop.py
+++ train_loop.py
@@ -2,6 +2,8 @@ import numpy as np
 from nn.nn import NN
 from config import config
 from mcts import UCTPlayGame
+import cPickle as pickle
+import os
 
 if __name__ == "__main__":
     """ Play a single game to the end using UCT for both players. 
@@ -11,6 +13,7 @@ if __name__ == "__main__":
     
     batch_data = {}
     batch_data['s'], batch_data['pi'], batch_data['z'] = ([], [], [])
+    data_path = os.path.join(config.save_path, 'batch_data_iter_{}.pkl')
 
     for iteration in range(config.n_iters):
         
@@ -25,8 +28,13 @@ if __name__ == "__main__":
         print "Finished collecting self-play data from iteration %d" % iteration
         
         nn.train((batch_data['s'], batch_data['pi'], batch_data['z']))
+        
+        print "Saving batch data..."
+        fn = data_path.format(iteration)
+        pickle.dump(batch_data, open(fn, 'wb'), pickle.HIGHEST_PROTOCOL)
+        print "Done saving {}".format(fn)
 
         print "Finished training on self-play data from iteration %d" % iteration
         
-        for k in batch_data.keys():
+        for k in batch_data.keys(): # remove oldest 25% from buffer
             batch_data[k] = batch_data[k][int(0.25*config.buffer_size):]
