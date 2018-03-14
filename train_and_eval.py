cfe5e7b7e1a6526da3aba6410e7a9b15e0dc3c0b
Hana Lee <leehana@stanford.edu>
2018-03-13 21:43:40 -0700

    fixed super dumb bug

--- train_and_eval.py
+++ train_and_eval.py
@@ -3,7 +3,7 @@ import subprocess
 from config import config
 
 def main():
-	for t in config.n_iters:
+	for t in range(config.n_iters):
 		subprocess.call('python train_loop.py', shell=True)
 		subprocess.call('python ./run_eval.py %d %d' % (config.n_eval_games, t+1), shell=True, cwd='match-wrapper/')
 		print "Finished one step of train + eval. Results are in match-wrapper/eval_results_%d.txt" % (t+1)
